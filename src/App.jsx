8b12c7c29f38a8ee1b9f54fa04cc2a6ac6d1a41b
Zeb Gambill <zebgambill@Zebs-MacBook-Air.local>
2023-04-12 13:43:08 -0400

    new features

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,23 @@ function App() {
   const [loading, setLoading] = useState(true)
   const [first150Pokemon, setFirst150Pokemon] = useState([])
   const [searchInput, setSearchInput] = useState('')
+  const [showData, setShowData] = useState(null)
 
   const fetchPokemon = async () => {
     const pokemonArray = []
     for (let i = 1; i <= 150; i++) {
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
       const data = await response.json()
+
       const pokemon = {
         id: data.id,
         name: data.name,
         image: data.sprites.front_default,
+        abilities: data.abilities[0].ability.name,
+        abilitiesCont: data.abilities[1]?.ability.name,
+        abilitiesCont2: data.abilities[2]?.ability.name,
       }
+
       pokemonArray.push(pokemon)
     }
 
@@ -36,8 +42,7 @@ function App() {
 
   const resetPokemon = () => {
     setSearchInput('')
-    fetchPokemon()
-    setLoading(true)
+    setShowData(null)
   }
 
   const filteredPokemon = first150Pokemon.filter((poke) =>
@@ -50,24 +55,46 @@ function App() {
 
   return (
     <div className="pokedex">
-      <h1 className="title">Pokédex </h1>
+      <h1>
+        <img src="src/assets/pokedeximg.webp" alt="pokedex text" />
+      </h1>
       <input
         type="text"
         placeholder="Search here"
         onChange={handleChange}
         value={searchInput}
+        className="input-box"
       />
       <button className="button" onClick={resetPokemon}>
         Reset
       </button>
 
       {filteredPokemon.map((poke, i) => {
-        const { name, image } = poke
+        const { name, id, image, abilities, abilitiesCont, abilitiesCont2 } =
+          poke
 
         return (
           <div key={i} className="pokemon-grid">
             <h2 className="poke-name">{name}</h2>
+
             <img className="pokedex img" src={image} alt="" />
+            <button
+              className="abilities-btn"
+              onClick={() => setShowData(showData === poke.id ? null : poke.id)}
+            >
+              Abilities
+            </button>
+            {showData === id && ( // only show the abilities for the selected Pokemon
+              <div>
+                <p className="abilities">{abilities}</p>
+                {abilitiesCont ? (
+                  <p className="abilities">{abilitiesCont}</p>
+                ) : null}
+                {abilitiesCont2 ? (
+                  <p className="abilities">{abilitiesCont2}</p>
+                ) : null}
+              </div>
+            )}
           </div>
         )
       })}
